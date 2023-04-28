870606da4e6c44b43eac181ba9bdab8bef4f0848
Enikeeva Aliya <aliaa@mail.ru>
2023-04-28 21:12:11 +0500

    Homework04

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import model.DynArray;
+import view.Menu;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Menu menu = new Menu();
+        menu.mainMenu();
+    }
+}
